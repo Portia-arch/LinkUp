e4567101be6ac358e1c0972f69356c6874b73b55
Portia-arch <porschetess@icloud.com>
2025-10-22 23:23:56 +0200

    fixed the folder structure, installed dependancies, firebase and expo CLI

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6C-gCyiCDfz_NsbO_aGQ4DYIBJ5gVBNE",
+  authDomain: "linkup-c1929.firebaseapp.com",
+  projectId: "linkup-c1929",
+  storageBucket: "linkup-c1929.firebasestorage.app",
+  messagingSenderId: "605771402121",
+  appId: "1:605771402121:web:5c73816cbb656d8134ef58"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
